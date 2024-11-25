8aa16a68337f10f227637272069091e1e77c48d0
EdgarAllenExile <riley.mercer4@gmail.com>
2024-11-25 19:09:01 +0800

    not too sure

--- lua/plugins/mini.nvim.lua
+++ lua/plugins/mini.nvim.lua
@@ -17,7 +17,7 @@ return { -- Collection of various small independent plugins/modules
     -- - sr)'  - [S]urround [R]eplace [)] [']
     require('mini.surround').setup()
     require('mini.icons').setup()
-    require('mini.comment').setup()
+    -- require('mini.comment').setup()
     -- Simple and easy statusline.
     --  You could remove this setup call if you don't like it,
     --  and try some other statusline plugin
