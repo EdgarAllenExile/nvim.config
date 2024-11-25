f1bf3eb010c2c459bb1272e0f0f5474b381ed6f0
Riley Mercer <riley.mercer4@gmail.com>
2024-11-25 15:41:30 +0800

    2024/11/25 - nvim Notes

--- lua/plugins/helpers/hardtime.lua
+++ lua/plugins/helpers/hardtime.lua
@@ -1,5 +1,12 @@
 return {
   'm4xshen/hardtime.nvim',
   dependencies = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim' },
-  opts = {},
+  opts = {
+    max_count = 5,
+    ---@type function
+    callback = function(text)
+      vim.notify(text, vim.log.levels.WARN, { title = 'hardtime' })
+      require('precognition').peek()
+    end,
+  },
 }
