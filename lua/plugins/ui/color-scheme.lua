520e99f4fa9c9e3332d75c5aff5c3d527c0abfb6
EdgarAllenExile <riley.mercer4@gmail.com>
2024-12-14 16:15:44 +0800

    Update nvim

---
+++ lua/plugins/ui/color-scheme.lua
@@ -0,0 +1,19 @@
+return {
+  'loctvl842/monokai-pro.nvim',
+  lazy = false,
+  priority = 1000,
+
+  config = function()
+    require('monokai-pro').setup {
+      transparent_background = false,
+      devicons = true,
+      filter = 'spectrum',
+      background_clear = {
+        -- 'neo-tree',
+        -- 'bufferline',
+      },
+    }
+    vim.cmd.hi 'Comment gui=none'
+    vim.cmd.colorscheme 'monokai-pro'
+  end,
+}
