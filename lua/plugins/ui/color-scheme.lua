2743ea4f796a9d2179be38dc1109d417e10d63d7
EdgarAllenExile <riley.mercer4@gmail.com>
2025-01-27 16:53:44 +0800

    Updates to plugins files

--- lua/plugins/ui/color-scheme.lua
+++ lua/plugins/ui/color-scheme.lua
@@ -1,61 +1,19 @@
 return {
-  {
-    'loctvl842/monokai-pro.nvim',
-    lazy = false,
-    priority = 1000,
-    keys = { { '<leader>C', '<cmd>MonokaiProSelect<cr>', desc = 'Select Monokai pro filter' } },
-    opts = {
-      transparent_background = false,
-      devicons = true,
-      filter = 'pro', -- classic | octagon | pro | machine | ristretto | spectrum
-      plugins = {},
-      --   override = function(c)
-      --     return {
-      --       -- ColorColumn = { bg = c.editor.background },
-      --       -- Mine
-      --       DashboardRecent = { fg = c.base.magenta },
-      --       DashboardProject = { fg = c.base.blue },
-      --       DashboardConfiguration = { fg = c.base.white },
-      --       DashboardSession = { fg = c.base.green },
-      --       DashboardLazy = { fg = c.base.cyan },
-      --       DashboardServer = { fg = c.base.yellow },
-      --       DashboardQuit = { fg = c.base.red },
-      --       IndentBlanklineChar = { fg = c.base.dimmed4 },
-      --       NeoTreeStatusLine = { link = 'StatusLine' },
-      --       -- mini.hipatterns
-      --       MiniHipatternsFixme = { fg = c.base.black, bg = c.base.red, bold = true }, -- FIXME
-      --       MiniHipatternsTodo = { fg = c.base.black, bg = c.base.blue, bold = true }, -- TODO
-      --       MiniHipatternsHack = { fg = c.base.black, bg = c.base.yellow, bold = true }, -- HACK
-      --       MiniHipatternsNote = { fg = c.base.black, bg = c.base.green, bold = true }, -- NOTE
-      --       MiniHipatternsWip = { fg = c.base.black, bg = c.base.cyan, bold = true }, -- WIP
-      --     }
-      --   end,
-      --   overridePalette = function(filter)
-      --     -- if filter == "pro" then
-      --     --   return {
-      --     --     dark2 = "#101014",
-      --     --     dark1 = "#16161E",
-      --     --     background = "#1A1B26",
-      --     --     text = "#C0CAF5",
-      --     --     accent1 = "#f7768e",
-      --     --     accent2 = "#7aa2f7",
-      --     --     accent3 = "#e0af68",
-      --     --     accent4 = "#9ece6a",
-      --     --     accent5 = "#0DB9D7",
-      --     --     accent6 = "#9d7cd8",
-      --     --     dimmed1 = "#737aa2",
-      --     --     dimmed2 = "#787c99",
-      --     --     dimmed3 = "#363b54",
-      --     --     dimmed4 = "#363b54",
-      --     --     dimmed5 = "#16161e",
-      --     --   }
-      --     -- end
-      --   end,
-    },
-    config = function(_, opts)
-      local monokai = require 'monokai-pro'
-      monokai.setup(opts)
-      monokai.load()
-    end,
+  'loctvl842/monokai-pro.nvim',
+  lazy = false,
+  priority = 1000,
+  opts = {
+    transparent_background = false,
+    devicons = true,
+    filter = 'pro', -- classic | octagon | pro | machine | ristretto | spectrum
+    plugins = {},
   },
+
+  config = function(_, opts)
+    local monokai = require 'monokai-pro'
+    monokai.setup(opts)
+    monokai.load()
+    vim.cmd.hi 'Comment gui=none'
+    vim.cmd.colorscheme 'monokai-pro'
+  end,
 }
