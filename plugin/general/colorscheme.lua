b01b318a3a482515a161267f903a50fcb78ef313
EdgarAllenExile <riley.mercer4@gmail.com>
2025-11-11 06:19:43 +0800

    Updates to nvim files

--- plugin/general/colorscheme.lua
+++ plugin/general/colorscheme.lua
@@ -3,39 +3,30 @@
 --   'https://github.com/loctvl842/monokai-pro.nvim',
 -- }
 
--- require('monokai-pro').setup {
---   filter = 'pro',
---   plugins = {},
---   override = function()
---     return {
---       FloatBorder = { fg = '#939293', bg = '#2d2a2e' }, -- This is TelescopeBorder highlight group
---       MiniTablineFill = { fg = '#fcfcfa' },
---     }
---   end,
--- }
-require('onedarkpro').setup {
-  colors = {
-    black = '#19181a',
-    dark1 = '#221f22',
-    bg = '#2d2a2e',
-    white = '#fcfcfa',
-    red = '#ff6188',
-    orange = '#fc9867',
-    yellow = '#ffd866',
-    green = '#a9dc76',
-    cyan = '#78dce8',
-    purple = '#ab9df2',
-    gray = '#c1c0c0',
-    gray2 = '#939293', -- border
-    comment = '#727072',
-    fg = '#5b595c',
-    blue = '#61afef',
-    highlight = '#e2be7d',
-    none = 'NONE',
+require('monokai-pro').setup {
+  filter = 'pro',
+  plugins = {},
+  override = function()
+    return {
+      FloatBorder = { fg = '#939293', bg = '#2d2a2e' }, -- This is TelescopeBorder highlight group
+      MiniTablineFill = { fg = '#fcfcfa' },
+      MarkviewPalette1 = { fg = '#ff6188' },
+      MarkviewPalette2 = { fg = '#ab9df2' },
+      MarkviewPalette3 = { fg = '#78dce8' },
+      MarkviewPalette4 = { fg = '#a9dc76' },
+    }
+  end,
+}
+require('catppuccin').setup {
+  color_overrides = {
+    latte = {
+      base = '#ff0000',
+      mantle = '#242424',
+      crust = '#474747',
+    },
   },
 }
-
-vim.cmd.colorscheme 'onedark'
+vim.cmd.colorscheme 'monokai-pro'
 
 local monokaipro = {
   black = '#19181a',
