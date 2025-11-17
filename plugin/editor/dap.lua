ef59b6bcd54d354102345d5d063c38dbddeebe09
EdgarAllenExile <riley.mercer4@gmail.com>
2025-11-17 12:35:03 +0800

    Updates to nvim files

--- plugin/editor/dap.lua
+++ plugin/editor/dap.lua
@@ -6,3 +6,24 @@ vim.pack.add {
 
 require('dapui').setup()
 -- require('dap').setup()
+
+local dap = require 'dap'
+dap.adapters.codelldb = {
+  type = 'executable',
+  command = 'codelldb', -- or if not in $PATH: "/absolute/path/to/codelldb"
+
+  -- On windows you may have to uncomment this:
+  -- detached = false,
+}
+dap.configurations.rust = {
+  {
+    name = 'Launch file',
+    type = 'codelldb',
+    request = 'launch',
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    cwd = '${workspaceFolder}',
+    stopOnEntry = false,
+  },
+}
