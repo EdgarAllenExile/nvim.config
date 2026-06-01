d9ca7ae651e9e310a4e85f4306be58b7e4259075
r-mercer <riley.mercer4@gmail.com>
2026-06-02 07:17:58 +0800

    Updates to nvim

--- plugin/editor/lang/typescript.lua
+++ plugin/editor/lang/typescript.lua
@@ -2,4 +2,20 @@ vim.pack.add {
   'https://github.com/pmizio/typescript-tools.nvim',
 }
 
-require('typescript-tools').setup()
+require('typescript-tools').setup {
+  settings = {
+    expose_as_code_action = 'all',
+    jsx_close_tag = {
+      enable = true,
+    },
+    tsserver_format_options = {
+      allowIncompleteCompletions = false,
+      allowRenameOfImportPath = false,
+    },
+    tsserver_file_preferences = {
+      includeCompletionsForModuleExports = true,
+      includeCompletionsWithInsertText = true,
+      includePackageJsonAutoImports = 'auto',
+    },
+  },
+}
